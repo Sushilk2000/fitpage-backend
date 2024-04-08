482c06d71cd4618f85df2f94e731e912e79fa8cb
SushilKulkarni <sushilkulkarni232@gmail.com>
2024-04-08 11:01:08 +0530

    few changes

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
+const stockRoute = require("./Routes/Stock");
 app.use(cors());
 app.use(express.json());
-app.use("/api/stocks", require("./Routes/Stock"));
-const port = process.env.PORT || 10000;
+app.use("/api/stocks", stockRoute);
+const port = 10000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
